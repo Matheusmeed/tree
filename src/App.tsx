ea5ed4cac65bf3ca1a7bc5cddc4bca582f158a5c
Matheus <mms.medeiros12@gmail.com>
2025-07-18 15:49:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TreeView from './components/TreeView';
+
+function App() {
+  return (
+    <>
+      <TreeView />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TreeView/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+const TreeView = () => {
+  return <Container></Container>;
+};
+
+export default TreeView;
